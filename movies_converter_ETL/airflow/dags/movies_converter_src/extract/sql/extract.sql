dfdf7b71fe8bfd7f4c36adb10a16b1aa791ab9ad
Dimk00z <dimk00z@gmail.com>
2022-03-01 11:49:53 +0300

    Fix sql

--- movies_converter_ETL/airflow/dags/movies_converter_src/extract/sql/extract.sql
+++ movies_converter_ETL/airflow/dags/movies_converter_src/extract/sql/extract.sql
@@ -1,3 +1,4 @@
-SELECT fw.film_id as fw_id, file_name, destination_path, source_path, source_resolution, ARRAY_AGG(DISTINCT ff.resolution) AS resolutions
+SELECT fw.film_id as fw_id, file_name, fw.destination_path, source_path, source_resolution, ARRAY_AGG(DISTINCT ff.resolution) AS resolutions
 FROM content.film_work fw, content.film_files ff
-WHERE fw.film_id = ff.film_id;
+WHERE fw.film_id = ff.film_id
+GROUP BY fw_id;
