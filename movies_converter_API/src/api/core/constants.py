5acedadcd3d462fbc21d48aa19ec5c389a9988a2
dmurd <d.murdashev@yandex.ru>
2022-03-05 14:14:15 +0300

    Fix issue from review

--- movies_converter_API/src/api/core/constants.py
+++ movies_converter_API/src/api/core/constants.py
@@ -1,6 +1,6 @@
-from fastapi import HTTPException, status
+from http import HTTPStatus
 
-DONE_ACTION = HTTPException(status_code=status.HTTP_200_OK)
-NOT_FOUND = HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-CREATE_ACTION = HTTPException(status_code=status.HTTP_201_CREATED)
-EXISTS_ACTION = HTTPException(status_code=status.HTTP_409_CONFLICT)
+DONE_ACTION = HTTPStatus.OK
+NOT_FOUND = HTTPStatus.NOT_FOUND
+CREATE_ACTION = HTTPStatus.CREATED
+EXISTS_ACTION = HTTPStatus.CONFLICT
--- movies_converter_API/src/api/db/connections.py
+++ movies_converter_API/src/api/db/connections.py
@@ -2,12 +2,14 @@ import sys
 import traceback
 
 import backoff
+import tortoise as tortoise
+
 from db.postgres.connection import close_postgres, init_postgres
 from loguru import logger
 
 
 def backoff_handler(details):
-    _, _, traceback_ = sys.exc_info()
+    traceback_ = sys.exc_info()[2]
     logger.debug(
         "backoff {wait:0.1f}s after {tries} tries "
         "calling function {target} with args {args} and kwargs {kwargs}".format(
@@ -22,7 +24,7 @@ def backoff_handler(details):
     on_backoff=backoff_handler,
     max_tries=3,
     max_time=60,
-    exception=Exception,
+    exception=tortoise.exceptions.DBConnectionError,
 )
 async def init_connections():
     await init_postgres()
